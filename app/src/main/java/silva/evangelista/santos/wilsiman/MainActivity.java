b8b2a4cd224fa9825f93764bb44941694040ea86
20211TIIMI0281 <wilsiman.evangelista.ifes@gmail.com>
2023-03-15 09:08:30 -0300

    Comentando o código

--- app/src/main/java/silva/evangelista/santos/wilsiman/MainActivity.java
+++ app/src/main/java/silva/evangelista/santos/wilsiman/MainActivity.java
@@ -14,14 +14,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Criação de um novo botão já declarado no arquivo xml pegando ele pelo id
         Button btnEnviar = findViewById(R.id.btnEnviar);
+        //vinculando o botão enviar com a classe onClick de forma que ela receba quando foi clicado
         btnEnviar.setOnClickListener(new View.OnClickListener() {
             @Override
+            //Classe criada para adicionar funcionalidade ao botão quando é clicado
             public void onClick(View view) {
+                //pegando campo de texto declarado no arquivo xml pelo id
                 EditText etDigiteAqui = findViewById(R.id.etDigiteAqui);
+                //pegando texto que foi digitado no campo de texto declarado
                 String textoDigitado = etDigiteAqui.getText().toString();
+                //criando uma intenção e passando como parâmetro a tela que está inicialmente a intenção e para onde as infromações irão
                 Intent intent = new Intent(MainActivity.this,NextActivity.class);
+                //passando como parâmetro para o intent a mensagem que irá aparecer
                 intent.putExtra("Texto", textoDigitado);
+                //iniciando a intenção
                 startActivity(intent);
             }
         });
--- app/src/main/java/silva/evangelista/santos/wilsiman/NextActivity.java
+++ app/src/main/java/silva/evangelista/santos/wilsiman/NextActivity.java
@@ -12,9 +12,13 @@ public class NextActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_next);
+        //declarando intent e pegando ela para ser manipulada
         Intent i = getIntent();
+        //declarando o texto digitado e pegando ele pelo nome
         String textoDigitado = i.getStringExtra("Texto");
+        //pegando caixa de texto criada no arquivo xml para ser manipulada
         TextView tvText = findViewById(R.id.tvText);
+        //pegando caixa de texto desta activity e colocando o texto digitado nele que foi recebido por meio da intent
         tvText.setText(textoDigitado);
     }
 }
